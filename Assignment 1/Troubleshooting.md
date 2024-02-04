21a5fac04a0fcf39f108e7aea0bde817b6c5cea5
Divy Patel <divy9881@gmail.com>
2024-02-03 18:57:58 -0600

    feat: add initial sorted data generator
    Signed-off-by: Divy Patel <divy9881@gmail.com>

--- Assignment 1/Troubleshooting.md
+++ Assignment 1/Troubleshooting.md
@@ -6,4 +6,10 @@ sudo chmod ugo+rwx /mnt/data/namenode
 sudo chmod ugo+rwx /mnt/data/datanode
 ```
 
+### Add hadoop exeecutables to env
+```
+export PATH=$PATH:hadoop-3.3.6/bin
+export PATH=$PATH:hadoop-3.3.6/sbin
+```
+
 Follow the instructions given on [Assignment Page](https://pages.cs.wisc.edu/~shivaram/cs744-sp24/assignment1.html)
---
+++ Assignment 1/pyspark.py
@@ -0,0 +1,29 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+import sys
+
+def generate_sorted_data(input_path, output_path):
+    data = spark.read.csv(input_path, header=True, inferSchema=True)
+
+    sorted_data = data.orderBy(col("cca2"), col("timestamp"))
+    # sorted_data.show()
+
+    sorted_data.write.csv(output_path, header=True)
+
+if __name__ == "__main__":
+    spark = SparkSession.builder \
+        .appName("Big-Data-Assignment-1") \
+        .config("spark.driver.memory", "30g") \
+        .config("spark.executor.memory", "30g") \
+        .config("spark.executor.cores", "5") \
+        .config("spark.task.cpus", "1") \
+        .getOrCreate()
+
+    if len(sys.argv) != 3:
+        print("Usage: spark-submit <script.py> <input_path> <output_path>")
+        sys.exit(1)
+
+    input_path = sys.argv[1]
+    output_path = sys.argv[2]
+    generate_sorted_data(input_path, output_path)
+    spark.stop()
