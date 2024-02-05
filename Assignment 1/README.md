bbaff9268e240d97212d04c1c4c7b60b77422cec
Divy Patel <divy9881@gmail.com>
2024-02-04 20:38:47 -0600

    fix: add README for enwiki articles graph dataset
    Signed-off-by: Divy Patel <divy9881@gmail.com>

--- Assignment 1/README.md
+++ Assignment 1/README.md
@@ -1,9 +1,24 @@
 ### Run sort.py Pyspark script
 ```
-~/spark-3.3.4-bin-hadoop3/bin/spark-submit --master=spark://10.10.1.1:7077 ~/Big-Data-Assignments/Assignment\ 1 sort.py hdfs://10.10.1.1:9000/export.csv hdfs://10.10.1.1:9000/final-result
+$ git clone https://github.com/divy9881/big-data-assignments
+$ wget http://pages.cs.wisc.edu/~shivaram/cs744-fa18/assets/export.csv
+$ hdfs dfs -copyFromLocal ./export.csv /export.csv
+$ ~/spark-3.3.4-bin-hadoop3/bin/spark-submit --master=spark://10.10.1.1:7077 ./Big-Data-Assignments/Assignment\ 1/sort.py hdfs://10.10.1.1:9000/export.csv hdfs://10.10.1.1:9000/final-result
 ```
 
-### Run web-BerkStan app
+### Run PageRank on web-BerkStan data
 ```
- ~/spark-3.3.4-bin-hadoop3/bin/spark-submit --master=spark://10.10.1.1:7077 ~/Big-Data-Assignments/Assignment\ 1/pagerank.py hdfs://10.10.1.1:9000/part-3/web-BerkStan.txt
+$ git clone https://github.com/divy9881/big-data-assignments
+$ wget https://snap.stanford.edu/data/web-BerkStan.txt.gz
+$ gunzip web-BerkStan.txt.gz
+$ hdfs dfs -mkdir /part-3
+$ hdfs dfs -copyFromLocal ./web-BerkStan.txt /part-3/web-BerkStan.txt
+$ ~/spark-3.3.4-bin-hadoop3/bin/spark-submit --master=spark://10.10.1.1:7077 ./Big-Data-Assignments/Assignment\ 1/pagerank.py hdfs://10.10.1.1:9000/part-3/web-BerkStan.txt
+```
+
+### Run PageRank on enwiki articles data
+```
+$ git clone https://github.com/divy9881/big-data-assignments
+$ hdfs dfs -put /proj/uwmadison744-s24-PG0/data-part3/enwiki-pages-articles/* /part-3/enwiki/
+$ ~/spark-3.3.4-bin-hadoop3/bin/spark-submit --master=spark://10.10.1.1:7077 ./Big-Data-Assignments/Assignment\ 1/pagerank.py hdfs://10.10.1.1:9000/part-3/enwiki/*
 ```
--- Assignment 1/pagerank.py
+++ Assignment 1/pagerank.py
@@ -24,36 +24,36 @@ def pagerank(spark, input_file_path):
 
     ranks = links.flatMap(assign_ranks)
 
-    f = open("counts.txt", "w+")
-    f.write(str(ranks.count()))
-    f.close()
+    # f = open("counts.txt", "w+")
+    # f.write(str(ranks.count()))
+    # f.close()
 
     links = links.groupByKey().mapValues(list)
 
     for iteration in range(10):
         links_ranks = links.join(ranks)
 
-        links_r = links_ranks.top(100)
-        f = open("links.txt", "w+")
-        for l in links_r:
-            f.write("{} \n".format(str(l)))
-        f.close()
+        # links_r = links_ranks.top(100)
+        # f = open("links.txt", "w+")
+        # for l in links_r:
+        #     f.write("{} \n".format(str(l)))
+        # f.close()
 
         contributions = links_ranks.flatMap(lambda page_links_rank: [(link, page_links_rank[1][1] / len(page_links_rank[1][0])) for link in page_links_rank[1][0]])
         
-        contributions_r = contributions.top(100)
-        f = open("contributions.txt", "w+")
-        for l in contributions_r:
-            f.write("{} \n".format(str(l)))
-        f.close()
+        # contributions_r = contributions.top(100)
+        # f = open("contributions.txt", "w+")
+        # for l in contributions_r:
+        #     f.write("{} \n".format(str(l)))
+        # f.close()
 
         ranks = contributions.reduceByKey(lambda x, y: x + y).mapValues(lambda rank: 0.15 + 0.85 * rank)
 
-    final_ranks = ranks.collect()
-    f = open("final_ranks.txt", "w+")
-    for page, rank in final_ranks:
-        f.write("Page: {page}, Rank: {rank} \n".format(page=page, rank=rank))
-    f.close()
+    # final_ranks = ranks.collect()
+    # f = open("final_ranks.txt", "w+")
+    # for page, rank in final_ranks:
+    #     f.write("Page: {page}, Rank: {rank} \n".format(page=page, rank=rank))
+    # f.close()
     
 
 if __name__ == "__main__":
