8425f2e48c4bce98401984dc9a5eba92f8a291b3
Venkata Abhijeeth Balabhadruni <bvabhi24@gmail.com>
2024-02-17 02:26:58 +0000

    update part3

--- Assignment 2/part3/part3.py
+++ Assignment 2/part3/part3.py
@@ -96,7 +96,6 @@ def main():
                                                     num_workers=2,
                                                     batch_size=batch_size,
                                                     sampler=sampler_d,
-                                                    shuffle=True,
                                                     pin_memory=True)
     test_set = datasets.CIFAR10(root="./data", train=False,
                                 download=True, transform=transform_test)
