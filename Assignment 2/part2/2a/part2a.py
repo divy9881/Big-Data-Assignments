e7a165d916a3318cb674327d6f26d5faf4d83cad
Venkata Abhijeeth Balabhadruni <bvabhi24@gmail.com>
2024-02-18 19:11:06 +0000

    modify if else for other ranked nodes

--- Assignment 2/part2/2a/part2a.py
+++ Assignment 2/part2/2a/part2a.py
@@ -39,13 +39,19 @@ def train_model(model, train_loader, optimizer, criterion, epoch):
             grad_list = [torch.zeros_like(p.grad) for _ in range(4)]
             if rank_of_node == 0:
                 dist.gather(p.grad, grad_list, group=group, async_op=False)
-                grad_sum = torch.zeros_like(p.grad)
-                for i in range(4):
-                    grad_sum += grad_list[i]
-                grad_mean = grad_sum / 4
-                scatter_list = [grad_mean for _ in range(4)]
+            else:
+                dist.gather(p.grad, [], group=group, async_op=False)
+
+            grad_sum = torch.zeros_like(p.grad)
+            for i in range(4):
+                grad_sum += grad_list[i]
+            grad_mean = grad_sum / 4
+            scatter_list = [grad_mean for _ in range(4)]
+            if rank_of_node == 0:
                 dist.scatter(p.grad, scatter_list, group=group, src=0, async_op=False)
-                print('Result gathered',dist.lst_of_gradients,type(dist.lst_of_gradients))
+            else:
+                dist.scatter(p.grad, [], group=group, src=0, async_op=False)
+            print('Result gathered',dist.lst_of_gradients,type(dist.lst_of_gradients))
         optimizer.step()
         if batch_idx % 20 == 0:
             print("Iteration Number: ", batch_idx, ", loss: ", train_loss.item())
